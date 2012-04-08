fe18743499918417055bc4df558dbe3497c80cac
Steve Liles <steveliles@gmail.com>
2012-04-08 11:41:18 +0100

    fleshing out the DoubleConverterTest

--- src/test/java/com/sjl/dsl4xml/support/convert/DoubleConverterTest.java
+++ src/test/java/com/sjl/dsl4xml/support/convert/DoubleConverterTest.java
@@ -1,5 +1,49 @@
 package com.sjl.dsl4xml.support.convert;
 
-public class DoubleConverterTest {
+import org.junit.*;
 
+public class DoubleConverterTest {
+	
+	@Test
+	public void canConvertDoubles() {
+		DoubleConverter _c = new DoubleConverter();
+		Assert.assertTrue(_c.canConvertTo(Double.class));
+	}
+	
+	@Test
+	public void convertsSmallOrdinalValues() {
+		DoubleConverter _c = new DoubleConverter();
+		Assert.assertEquals(new Double(1), _c.convert("1"));
+	}
+	
+	@Test
+	public void convertsLargeOrdinalValues() {
+		DoubleConverter _c = new DoubleConverter();
+		Assert.assertEquals(new Double(Integer.MAX_VALUE + 2L), _c.convert("2147483649"));
+	}
+	
+	@Test
+	public void convertsSmallRealValues() {
+		DoubleConverter _c = new DoubleConverter();
+		Assert.assertEquals(new Double(1), _c.convert("1.0"));
+	}
+	
+	@Test
+	public void convertsLargeRealValues() {
+		DoubleConverter _c = new DoubleConverter();
+		Assert.assertEquals(new Double((double)Integer.MAX_VALUE + 0.012345f), _c.convert("2147483647.012345"));
+	}
+	
+	@Test
+	public void convertsNullToNull() {
+		DoubleConverter _c = new DoubleConverter();
+		Assert.assertEquals(null, _c.convert(null));
+	}
+	
+	@Test
+	public void convertsEmptyStringToNull() {
+		DoubleConverter _c = new DoubleConverter();
+		Assert.assertEquals(null, _c.convert(""));
+	}
+	
 }
