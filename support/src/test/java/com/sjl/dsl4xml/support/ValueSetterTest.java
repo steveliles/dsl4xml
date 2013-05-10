e82b6d620ae5bb7411da4763c3ba6518b1875e0f
Steve Liles <steveliles@gmail.com>
2013-05-10 10:43:22 +0100

    refactoring towards a single consistent dsl for xml parsing, and likewise for json parsing

---
+++ support/src/test/java/com/sjl/dsl4xml/support/ValueSetterTest.java
@@ -0,0 +1,131 @@
+package com.sjl.dsl4xml.support;
+
+import org.jmock.*;
+import org.jmock.lib.legacy.ClassImposteriser;
+import org.junit.*;
+
+import com.sjl.dsl4xml.*;
+
+public class ValueSetterTest {
+
+	private HasConverters converters;
+	private TestModel model;
+	private StringConverter<Integer> integerConverter;
+	private Mockery mockery;
+
+	@SuppressWarnings("unchecked")
+	@Before
+	public void setup() {
+        mockery = new Mockery();
+        mockery.setImposteriser(ClassImposteriser.INSTANCE);
+		converters = mockery.mock(HasConverters.class);
+		model = mockery.mock(TestModel.class);
+		integerConverter = mockery.mock(StringConverter.class);
+	}
+	
+	@After
+	public void teardown() {
+		mockery.assertIsSatisfied();
+	}
+	
+	@Test
+	public void usesSuppliedConvertersToPerformTypeConversion() {
+		mockery.checking(new Expectations() {{
+			oneOf(converters).getConverter(Integer.class); // parameter to the setFieldOne method is of type Integer
+		}});
+		
+		new ValueSetter(converters, TestModel.class, "fieldOne");
+	}
+
+	@Test
+	public void introspectsModelClassToFindAndInvokeSetterMethods() {
+		mockery.checking(new Expectations() {{
+			oneOf(converters).getConverter(Integer.class); // parameter to the setFieldOne method is of type Integer
+			will(returnValue(integerConverter));
+			
+			oneOf(integerConverter).convert("53");
+			will(returnValue(new Integer(53)));
+			
+			oneOf(model).setFieldOne(new Integer(53));
+		}});
+		
+		ValueSetter _s = new ValueSetter(converters, TestModel.class, "fieldOne");
+		_s.invoke("fieldOne", model, "53");
+	}
+	
+	@Test
+	public void introspectsModelClassToFindAndInvokeAddMethods() {
+		mockery.checking(new Expectations() {{
+			oneOf(converters).getConverter(Integer.class); // parameter to the setFieldOne method is of type Integer
+			will(returnValue(integerConverter));
+			
+			oneOf(integerConverter).convert("17");
+			will(returnValue(new Integer(17)));
+			
+			oneOf(model).addFieldTwo(new Integer(17));
+		}});
+		
+		ValueSetter _s = new ValueSetter(converters, TestModel.class, "fieldTwo");
+		_s.invoke("fieldTwo", model, "17");
+	}
+	
+	@Test
+	public void introspectsModelClassToFindAndInvokeInsertMethods() {
+		mockery.checking(new Expectations() {{
+			oneOf(converters).getConverter(Integer.class); // parameter to the setFieldOne method is of type Integer
+			will(returnValue(integerConverter));
+			
+			oneOf(integerConverter).convert("98");
+			will(returnValue(new Integer(98)));
+			
+			oneOf(model).insertFieldThree(new Integer(98));
+		}});
+		
+		ValueSetter _s = new ValueSetter(converters, TestModel.class, "fieldThree");
+		_s.invoke("fieldThree", model, "98");
+	}
+	
+	@Test
+	public void throwsGoodExceptionWhenNoAvailableSetterMethod() {
+		try {
+			new ValueSetter(converters, TestModel.class, "fieldSix");
+			Assert.fail("Expected an exception");
+		} catch (NoSuitableMethodException anExc) {
+			// good
+		}
+	}
+	
+	@Test
+	public void throwsGoodExceptionWhenNoArgSetterMethod() {
+		try {
+			new ValueSetter(converters, TestModel.class, "fieldFour");
+			Assert.fail("Expected an exception");
+		} catch (NoSuitableMethodException anExc) {
+			// good
+		}
+	}
+	
+	@Test
+	public void throwsGoodExceptionWhenMultiArgSetterMethod() {
+		try {
+			new ValueSetter(converters, TestModel.class, "fieldFive");
+			Assert.fail("Expected an exception");
+		} catch (NoSuitableMethodException anExc) {
+			// good
+		}
+	}
+	
+	public interface TestModel {
+		public Integer getFieldOne();
+		public Integer getFieldTwo();
+		public Integer getFieldThree();
+
+		public void setFieldOne(Integer aValue);
+		public void addFieldTwo(Integer aValue);
+		public void insertFieldThree(Integer aValue);
+		
+		public void setFieldFour(); // not usable
+		public void setFieldFive(Integer aParam1, Integer aParam2); // not usable
+	}
+	
+}
---
+++ xml/pull/src/test/java/com/sjl/dsl4xml/NamedAttributesReaderTest.java
@@ -0,0 +1,79 @@
+package com.sjl.dsl4xml;
+
+import org.jmock.*;
+import org.jmock.lib.legacy.ClassImposteriser;
+import org.junit.*;
+
+import com.sjl.dsl4xml.pull.*;
+import com.sjl.dsl4xml.support.*;
+
+public class NamedAttributesReaderTest {
+
+	private ReadingContext ctx;
+	private TestModel model;
+	private StringConverter<String> strConverter;
+	private StringConverter<Integer> intConverter;
+	
+	private Mockery mockery;
+	
+	@SuppressWarnings("unchecked")
+	@Before
+	public void setup() {
+		mockery = new Mockery();
+		mockery.setImposteriser(ClassImposteriser.INSTANCE);
+		ctx = mockery.mock(ReadingContext.class);
+		model = mockery.mock(TestModel.class);
+		strConverter = (StringConverter<String>)mockery.mock(StringConverter.class, "string-converter");
+		intConverter = (StringConverter<Integer>)mockery.mock(StringConverter.class, "int-converter");
+	}
+	
+	@After
+	public void tearDown() {
+		mockery.assertIsSatisfied();
+	}
+	
+	@Test
+	public void readsSingleAttributeByName() {
+		mockery.checking(new Expectations(){{
+			oneOf(ctx).isStartTag(); will(returnValue(true));
+			oneOf(ctx).getAttributeValue("stringAttr"); will(returnValue("value"));
+			oneOf(ctx).getConverter(String.class); will(returnValue(strConverter));
+			
+			oneOf(ctx).peek(); will(returnValue(model));
+			
+			oneOf(strConverter).convert("value"); will(returnValue("value"));
+			oneOf(model).setStringAttr("value");
+		}});
+		
+		NamedAttributesReader _nar = new NamedAttributesReader("stringAttr");
+		_nar.read(ctx);
+	}
+	
+	@Test
+	public void readsMultipleAttributesByName() {
+		mockery.checking(new Expectations(){{
+			oneOf(ctx).isStartTag(); will(returnValue(true));
+			oneOf(ctx).getAttributeValue("stringAttr"); will(returnValue("value"));
+			oneOf(ctx).getAttributeValue("intAttr"); will(returnValue("12"));
+			oneOf(ctx).getConverter(String.class); will(returnValue(strConverter));
+			oneOf(ctx).getConverter(Integer.TYPE); will(returnValue(intConverter));
+			
+			allowing(ctx).peek(); will(returnValue(model));
+			
+			oneOf(strConverter).convert("value"); will(returnValue("value"));
+			oneOf(model).setStringAttr("value");
+			
+			oneOf(intConverter).convert("12"); will(returnValue(12));
+			oneOf(model).setIntAttr(12);
+		}});
+		
+		NamedAttributesReader _nar = new NamedAttributesReader("stringAttr", "intAttr");
+		_nar.read(ctx);
+	}
+	
+	interface TestModel {
+		public void setStringAttr(String aValue);
+		public void setIntAttr(int aValue);
+	}
+	
+}
