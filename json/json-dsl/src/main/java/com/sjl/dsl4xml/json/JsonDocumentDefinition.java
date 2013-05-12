a2174f4aeb4d2323db1017db59ee400c35b7959b
Steve Liles <steveliles@gmail.com>
2013-05-12 14:22:04 +0100

    beginning to work through implementation of JsonDocumentDefinition

--- json/json-dsl/src/main/java/com/sjl/dsl4xml/json/JsonDocumentDefinition.java
+++ json/json-dsl/src/main/java/com/sjl/dsl4xml/json/JsonDocumentDefinition.java
@@ -2,6 +2,7 @@ package com.sjl.dsl4xml.json;
 
 import com.sjl.dsl4xml.Converter;
 import com.sjl.dsl4xml.TypeSafeConverter;
+import com.sjl.dsl4xml.support.StringConverter;
 import com.sjl.dsl4xml.support.convert.*;
 
 import java.util.ArrayList;
@@ -41,6 +42,9 @@ public class JsonDocumentDefinition<T> implements DocumentDefinition<T> {
 
     @Override
     public <F> ConverterRegistration<F,Object> converting(Class<F> aToConvert) {
+        if (document != null)
+            throw new IllegalStateException("document already defined - too late to register converters!");
+
         return newConverterRegistration(aToConvert);
     }
 
@@ -60,83 +64,204 @@ public class JsonDocumentDefinition<T> implements DocumentDefinition<T> {
     }
 
     @Override
-    public Document<T> mapping(Class<? extends T> aType) {
+    public Document<T> mapping(final Class<? extends T> aType) {
         if (document != null)
             throw new IllegalStateException("target type is already set - don't call 'mapping' more than once!");
 
         return document = new Document<T>(){
+            private Class<?> intermediate;
+            private List<Content<?>> content;
+            private Converter<?,? extends T> converter;
+
             @Override
             public Document<T> with(Content<?>... aContent) {
-                return null; // todo
+                content = Arrays.asList(aContent);
+                return this;
             }
 
             @Override
             public <I> Document<I> via(Class<I> anIntermediateType) {
-                return null; // todo
+                intermediate = anIntermediateType;
+                converter = getConverter(anIntermediateType, aType);
+                return (Document<I>)this;
             }
 
             @Override
             public <I> Document<I> via(Class<I> anIntermediateType, Converter<I, ? extends T> aConverter) {
-                return null;  // todo
+                intermediate = anIntermediateType;
+                converter = aConverter;
+                return (Document<I>)this;
             }
         };
     }
 
     @Override
     public <R> NamedObject<R> object(String aName) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return object(alias(aName, aName), null);
     }
 
     @Override
     public <R> NamedObject<R> object(Name aName) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return object(aName, null);
     }
 
     @Override
     public <R> NamedObject<R> object(String aName, Class<R> aType) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return object(alias(aName, aName), aType);
     }
 
     @Override
-    public <R> NamedObject<R> object(Name aName, Class<R> aType) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    public <R> NamedObject<R> object(final Name aName, final Class<R> aType) {
+        return new NamedObject<R>(){
+            private List<Content<?>> content;
+            private Class<?> intermediate;
+            private Converter<?,R> converter;
+
+            @Override
+            public <I> NamedObject<I> via(Class<I> anIntermediateType) {
+                getConverter(anIntermediateType, aType);
+                intermediate = anIntermediateType;
+                return (NamedObject<I>) this;
+            }
+
+            @Override
+            public <I> NamedObject<I> via(Class<I> anIntermediateType, Converter<I, R> aConverter) {
+                intermediate = anIntermediateType;
+                converter = aConverter;
+                return (NamedObject<I>) this;
+            }
+
+            @Override
+            public NamedObject<R> with(Content<?>... aContent) {
+                content = Arrays.asList(aContent);
+                return this;
+            }
+
+            @Override
+            public Name getName() {
+                return aName;
+            }
+        };
     }
 
     @Override
-    public <R> UnNamedObject<R> object(Class<R> aType) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    public <R> UnNamedObject<R> object(final Class<R> aType) {
+        return new UnNamedObject<R>(){
+            private List<Content<?>> content;
+            private Converter<?,R> converter;
+            private Class<?> intermediate;
+
+            @Override
+            public <I> UnNamedObject<I> via(Class<I> anIntermediateType) {
+                intermediate = anIntermediateType;
+                converter = getConverter(intermediate, aType);
+                return (UnNamedObject<I>)this;
+            }
+
+            @Override
+            public <I> UnNamedObject<I> via(Class<I> anIntermediateType, Converter<I, R> aConverter) {
+                intermediate = anIntermediateType;
+                converter = aConverter;
+                return (UnNamedObject<I>)this;
+            }
+
+            @Override
+            public UnNamedObject<R> with(Content<?>... aContent) {
+                content = Arrays.asList(aContent);
+                return this;
+            }
+
+            @Override
+            public Name getName() {
+                return Name.MISSING;
+            }
+        };
     }
 
     @Override
     public <R> NamedArray<R> array(String aName) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return array(alias(aName, aName), null);
     }
 
     @Override
     public <R> NamedArray<R> array(Name aName) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return array(aName, null);
     }
 
     @Override
     public <R> NamedArray<R> array(String aName, Converter<List, R> aConverter) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return array(alias(aName, aName), aConverter);
     }
 
     @Override
     public <R> NamedArray<R> array(Name aName, Converter<List, R> aConverter) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return new NamedArray(){
+            @Override
+            public NamedArray of(Class<?> aConvertableType) {
+                return null;  // TODO
+            }
+
+            @Override
+            public NamedArray of(UnNamedProperty<?> aContent) {
+                return null;  // TODO
+            }
+
+            @Override
+            public NamedArray of(UnNamedObject<?> aContent) {
+                return null;  // TODO
+            }
+
+            @Override
+            public NamedArray of(UnNamedArray<?> aContent) {
+                return null;  // TODO
+            }
+
+            @Override
+            public Name getName() {
+                return null;  // TODO
+            }
+        };
     }
 
     @Override
     public <R> UnNamedArray<R> array() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return array(new Converter<List,R>(){
+            @Override
+            public R convert(List aFrom) {
+                return (R)aFrom;
+            }
+        });
     }
 
     @Override
     public <R> UnNamedArray<R> array(Converter<List, R> aConverter) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
+        return new UnNamedArray<R>(){
+            @Override
+            public UnNamedArray<R> of(Class<?> aConvertableType) {
+                return null;  // TODO
+            }
 
+            @Override
+            public UnNamedArray<R> of(UnNamedProperty<?> aContent) {
+                return null;  // TODO
+            }
+
+            @Override
+            public UnNamedArray<R> of(UnNamedObject<?> aContent) {
+                return null;  // TODO
+            }
+
+            @Override
+            public UnNamedArray<R> of(UnNamedArray<?> aContent) {
+                return null;  // TODO
+            }
+
+            @Override
+            public Name getName() {
+                return null;  // TODO
+            }
+        };
+    }
 
     @Override
     public <R> UnNamedProperty<R> property(Class<R> aClass) {
@@ -205,5 +330,19 @@ public class JsonDocumentDefinition<T> implements DocumentDefinition<T> {
                 return converter;
             }
         };
+    }
+
+    public <T> StringConverter<T> getConverter(Class<T> aTo) {
+        return (StringConverter<T>) getConverter(String.class, aTo);
+    }
+
+    public <F,T> TypeSafeConverter<F,T> getConverter(Class<F> aFromType, Class<T> aToType) {
+        for (TypeSafeConverter<?,?> _c : converters) {
+            if ((_c.canConvertFrom(aFromType)) && (_c.canConvertTo(aToType))) {
+                return (TypeSafeConverter<F,T>) _c;
+            }
+        }
+
+        throw new RuntimeException("no converter registered that can convert from " + aFromType + " to " + aToType);
     }
 }
--- json/json-dsl/src/main/java/com/sjl/dsl4xml/json/Name.java
+++ json/json-dsl/src/main/java/com/sjl/dsl4xml/json/Name.java
@@ -2,6 +2,18 @@ package com.sjl.dsl4xml.json;
 
 public interface Name {
 
+    public static final Name MISSING = new Name(){
+        @Override
+        public String getName() {
+            return "";
+        }
+
+        @Override
+        public String getAlias() {
+            return "";
+        }
+    };
+
     public String getName();
 
     public String getAlias();
